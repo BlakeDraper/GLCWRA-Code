7b27f06ebfa5c21096924aebe538f69c2454bd0a
Draper <bdraper@usgs.gov>
2016-09-23 16:10:42 -0500

    created var for easier path replace;tweak to time print statement

--- General Restoration Index/CalculateIndexWeb_params.py
+++ General Restoration Index/CalculateIndexWeb_params.py
@@ -5,9 +5,14 @@ Last Edit: 9/20/2016
 Seeks to define a function that performs the restoration index calculation on a
 given area given a set of 7 rasters (parameter 0 through parameter 6) and weight associated with each.
 """
-#test weight order: wlera 10;5;7.5;2.5;7.5;2.5
+#test weight order: wlera '10;5;7.5;2.5;7.5;2.5'
 
 import arcpy
+
+#inputRootPath = 'C:/Workspace/IndexTest/Data/WLERA_Raster_Data/'
+inputRootPath = 'D:/Projects/Scripts/GLCWRA/GLCWRAIndexCalculatorPy'
+#outPath = 'C:/Workspace/IndexTest/PythonOut'
+outPath = 'D:/Projects/Scripts/GLCWRA/GLCWRAIndexCalculatorPy/IndexCalcOutput'
 
 # Check out the spatial analysis extension
 arcpy.CheckOutExtension('Spatial')
@@ -23,13 +28,13 @@ studyArea = arcpy.GetParameterAsText(0)
 #Checks to see whether studyArea is one of the predefined variables. Prints an error out if it is not.
 # Setting p0 should fail if studyArea is not an appropriate value.
 if studyArea == 'wlera':
-    base_path = 'C:/Workspace/IndexTest/Data/WLERA_Raster_Data/WLERA_'
+    base_path = inputRootPath + '/WLERA_Raster_Data/WLERA_'
 
 elif studyArea == 'crsra':
-    base_path = 'C:/Workspace/IndexTest/Data/CRSRA_Raster_Data/CRSRA_'
+    base_path =  inputRootPath + '/CRSRA_Raster_Data/CRSRA_'
 
 elif studyArea == 'sbra':
-    base_path = 'C:/Workspace/IndexTest/Data/SBRA_Raster_Data/SBRA_'
+    base_path =  inputRootPath + '/SBRA_Raster_Data/SBRA_'
 
 else:
     print('Error, ' + studyArea + ' is not a valid study area.')
@@ -48,6 +53,7 @@ p6 = arcpy.Raster(base_path + 'P6_Landuse.tif')
 
 #capture the weights string to use as parameter
 paramWeights = arcpy.GetParameterAsText(1)
+print("paramWeights from GetParameterAsText:" + paramWeights)
 #parse out weight values delimited with semicolon (arcgis requirement)
 paramWeightList = paramWeights.split(";")
 print("Weights provided: " + paramWeights)
@@ -103,12 +109,13 @@ def getIndex(p0, p1, p2, p3, p4, p5, p6):
 def main(studyArea):
     t0 = time.clock()
     index_score = getIndex(p0, p1, p2, p3, p4, p5, p6)
-    outPath = 'C:/Workspace/IndexTest/PythonOut'
+    #outPath = 'C:/Workspace/IndexTest/PythonOut'
     # Creates a copy of the raster created from getIndex -- this should be the only output file. Changes some characteristics of the raster, like pixel_type
     arcpy.CopyRaster_management(index_score, outPath + '/' + studyArea + 'Index.tif', pixel_type='8_BIT_SIGNED',
                                 nodata_value='-128')
     print('The restoration index has been placed in ' + outPath + '.')
-    print('This process took apporximately ' + str(time.clock() - t0) + ' seconds to run.')
+    #print('This process took apporximately ' + str(time.clock() - t0) + ' seconds to run.')
+    print('This process took apporximately ' + "%.2f" % (time.clock() - t0)+ ' seconds to run.')
 
 
 main(studyArea)
